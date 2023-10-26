18b411b865966cd124b2a018f6333b999d6193a3
Scott <ScottWilliamsSolutions@gmail.com>
2023-10-25 21:01:30 -0400

    formatting finishing touches

--- src/HybridCar.java
+++ src/HybridCar.java
@@ -17,7 +17,7 @@ public class HybridCar extends Car{
 
     @Override
     public void startEngine() {
-        System.out.printf("Push to Start pressed, center console gently illuminates and displays 'Hello Watson', battery at %d Percent, all %d firing, Engine gently vrooooms%n", batteryLevel, cylinders);
+        System.out.printf("Push to Start pressed, center console gently illuminates and displays 'Hello Watson', battery at %d Percent, all %d cylinders firing, Engine gently vrooooms%n", batteryLevel, cylinders);
     }
 
     @Override
