c64186b7e6ca860dd141c39a19d0dc2d7c4ed714
Scott <ScottWilliamsSolutions@gmail.com>
2023-10-25 20:19:02 -0400

    Car class and new method tested

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car("Lamborghini");
 
+        peelOff(car);
     }
 
     public static void peelOff(Car car) {
