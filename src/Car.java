483517e4c6f949ce8a54c718996e0fb0fb9009c2
Scott <ScottWilliamsSolutions@gmail.com>
2023-10-25 20:03:23 -0400

    Car Template

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,16 @@
 public class Car {
+
+    private String description;
+
+    public void startEngine() {
+
+    }
+
+    public void drive() {
+
+    }
+
+    protected void runEngine() {
+
+    }
 }
